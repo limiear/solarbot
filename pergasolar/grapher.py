48f49e23cee9e9ff23cf6a431b666076728519f4
eloy <eloy.colell@gmail.com>
2015-03-25 12:36:58 -0300

    It rotate the radiation map to match with the known view angle.

--- pergasolar/grapher.py
+++ pergasolar/grapher.py
@@ -1,13 +1,15 @@
 from netcdf import netcdf as nc
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def draw(filepattern, filename):
+    invert_p = lambda m: np.rot90(np.rot90(m))
     with nc.loader(filepattern) as root:
         data = nc.getvar(root, 'globalradiation')[-1,:,:]
         y, x = data.shape
         plt.figure(figsize=(x/20, y/20))
-        img = plt.imshow(data)
+        img = plt.imshow(invert_p(data))
         img.set_clim(0, 1300)  # data.max())
         title = root.files[-1].split('/')[-1]
         plt.title(title)

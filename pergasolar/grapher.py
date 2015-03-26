39d76ff1c263b1a7cb849c26f80298f7827a87fd
eloy <eloy.colell@gmail.com>
2015-03-26 19:10:10 -0300

    It removed the innecesary rotation.

--- pergasolar/grapher.py
+++ pergasolar/grapher.py
@@ -1,15 +1,13 @@
 from netcdf import netcdf as nc
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 def draw(filepattern, filename, title):
-    invert_p = lambda m: np.rot90(np.rot90(m))
     with nc.loader(filepattern) as root:
         data = nc.getvar(root, 'globalradiation')[-1,:,:]
         y, x = data.shape
         plt.figure(figsize=(x/20, y/20))
-        img = plt.imshow(invert_p(data))
+        img = plt.imshow(data)
         img.set_clim(0, 1300)  # data.max())
         plt.title(title)
         plt.colorbar()
